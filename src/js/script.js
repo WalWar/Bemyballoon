9015786561f13568ef2731c24621fba7a22abbd2
ValeriyGor <valeriy.gorbachevskiy@gmail.com>
2018-02-09 18:21:37 +0200

    add menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,27 @@
+$(document).ready(function(){
+    
+  $('.parent-elem').hover(function () {
+     clearTimeout($.data(this,'timer'));
+     $('ul.second-level',this).stop(true,true).slideDown(250);
+  }, function () {
+    $.data(this,'timer', setTimeout($.proxy(function() {
+      $('ul.second-level',this).stop(true,true).hide(250);
+    }, this), 50));
+  });
+
+    $('.parent-item').hover(function () {
+     clearTimeout($.data(this,'timer'));
+     $('ul.third-level',this).stop(true,true).show(250);
+  }, function () {
+    $.data(this,'timer', setTimeout($.proxy(function() {
+      $('ul.third-level',this).stop(true,true).hide(250);
+    }, this), 50));
+  });
+
+
+});
 
 
 
 
-
+
